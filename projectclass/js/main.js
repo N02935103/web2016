418026f83a4fd359c386f01632fd6766c623ebd9
kenny kadilov <ken_lox@kennys-MacBook-Pro.local>
2016-02-18 19:56:53 -0500

    try it

---
+++ projectclass/js/main.js
@@ -0,0 +1,24 @@
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+  if(xhr.readyState === 4) {
+    var file = JSON.parse(xhr.responseText);
+    var statusHTML = '<tr>';
+    for (var i=0; i<file.length; i += 1) {
+    	
+      statusHTML += '<td>';
+      statusHTML += file[i].FirstName;
+      statusHTML += '</td>';
+      statusHTML += '<td>';
+      statusHTML += file[i].LastName;
+      statusHTML += '</td>';
+      statusHTML += '<td>';
+      statusHTML += '<a href="http://www.w3schools.com/html/">advise</a>';
+      statusHTML += '</td>';
+      statusHTML += '</tr>';
+      
+    }
+    document.getElementById('studentList').innerHTML =statusHTML;
+  }
+};
+xhr.open('GET', 'data/file.json');
+xhr.send();
